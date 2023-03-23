ad88e2252ca02f3dd426157c52f0dfbe79a20ed7
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-03-22 21:29:59 -0300

    Add files via upload

---
+++ 2023-programacao-2-ia21/README.md
@@ -0,0 +1,4 @@
+# 2023-programacao-2-ia21
+
+## Aluna: Elane Souza de Oliveira
+## Repositório destinado à disciplina de "Programação II"
---
+++ 2023-programacao-2-ia21/server/src/routes/tarefas.routes.ts
@@ -0,0 +1,7 @@
+import {Router} from "express"
+
+const router = Router() /*Rota especifica*/
+
+/*Rotas das tarefas*/
+
+export default router /*Exportando as rotas*/
