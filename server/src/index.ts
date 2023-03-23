69fa8ae02ec109062edc4145458812f692baca51
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-03-22 21:31:38 -0300

    Delete server directory

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import routes from "./routes/index.routes"
-
-const PORT = 3000
-const app = express()
-
-app.use(routes)
-
-app.listen(PORT, () => console.log(`⚡ server is running, on port ${PORT}`))
