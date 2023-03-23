37632ecf30da6912cd4ebb8c159004eb4722cf68
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-03-22 21:28:27 -0300

    Delete server directory

--- server/src/database.ts
+++
@@ -1,6 +0,0 @@
-/*Possibilita a criação e utilização do sql */
-/*De fato conecta o banco* | 'async' é assincrona | 'await' sincrona/
-
-
-
-/**Função que conecta com o banco*/
--- server/src/routes/tarefas.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from "express"
-
-const router = Router() /*Rota especifica*/
-
-/*Rotas das tarefas*/
-
-export default router /*Exportando as rotas*/
